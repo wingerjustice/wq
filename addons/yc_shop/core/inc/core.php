05de91cc626c3dc44edfc51cf26eba35197e2710
snail <254232364@qq.com>
2017-02-18 20:23:53 +0800

    修改底层结构

--- addons/yc_shop/core/inc/core.php
+++ addons/yc_shop/core/inc/core.php
@@ -17,54 +17,27 @@ class Core extends WeModuleSite {
     public $footer = array();
     public $header = null;
 
-    public function template($filename, $type = TEMPLATE_INCLUDEPATH){
+
+    /**
+     * 加载html 文件函数
+     * @$filename 文件夹名字
+     * @$file 文件名字 不填则默认index文件
+    */
+    public function template($filename,$file = ''){
         global $_W;
         $name = strtolower($this->modulename);
         $defineDir = dirname($this->__define);
-        if (defined('IN_SYS')) {
-            $source = IA_ROOT . "/web/themes/{$_W['template']}/{$name}/{$filename}.html";
-            $compile = IA_ROOT . "/data/tpl/web/{$_W['template']}/{$name}/{$filename}.tpl.php";
-            if (!is_file($source)) {
-                $source = IA_ROOT . "/web/themes/default/{$name}/{$filename}.html";
-            }
-            if (!is_file($source)) {
-                $source = IA_ROOT . "/addons/{$name}/template/{$filename}.html";
-            }
-            if (!is_file($source)) {
-                $source = IA_ROOT . "/web/themes/{$_W['template']}/{$filename}.html";
-            }
-            if (!is_file($source)) {
-                $source = IA_ROOT . "/web/themes/default/{$filename}.html";
-            }
-            if (!is_file($source)) {
-                $explode = explode('/', $filename);
-                $temp = array_slice($explode, 1);
-                $source = IA_ROOT . "/addons/{$name}/plugin/" . $explode[0] . "/template/" . implode('/', $temp) . ".html";
-            }
-        } else {
-            $source = IA_ROOT . "/app/themes/{$_W['template']}/{$name}/{$filename}.html";
-            $compile = IA_ROOT . "/data/tpl/app/{$_W['template']}/{$name}/{$filename}.tpl.php";
-            # 自定义变量
-            if(!is_file($source)) {
-                $source = IA_ROOT . "/addons/{$name}/template/mobile/{$filename}.html";
-            }
+        $file = empty($file) ? '/index' :".'/'$file";
 
-            if(!is_file($source)) {
-                $source = IA_ROOT . "/app/themes/default/{$name}/{$filename}.html";
-            }
-            if(!is_file($source)) {
-                $source = $defineDir . "/template/mobile/{$filename}.html";
-            }
-            if(!is_file($source)) {
-                $source = IA_ROOT . "/app/themes/{$_W['template']}/{$filename}.html";
-            }
-            if(!is_file($source)) {
-                if (in_array($filename, array('header', 'footer', 'slide', 'toolbar', 'message'))) {
-                    $source = IA_ROOT . "/app/themes/default/common/{$filename}.html";
-                } else {
-                    $source = IA_ROOT . "/app/themes/default/{$filename}.html";
-                }
-            }
+        if (defined('IN_SYS')) {
+            #定义插件 模版目录
+            $source = $defineDir.'/template/web/'.$filename.$file.'.html';
+            $compile = IA_ROOT . "/data/tpl/web/{$_W['template']}/{$name}/{$filename}.tpl.php";
+
+        } else {
+            #定义插件 模版目录
+            $source = $defineDir.'/template/mobile/'.$filename.$file.'.html';
+            $compile = IA_ROOT . "/data/tpl/app/{$_W['template']}/{$name}/{$filename}.tpl.php";
         }
         if (!is_file($source)) {
             die( "Error: template source '{$filename}' is not exist!" );
---
+++ addons/yc_shop/core/inc/define.php
@@ -0,0 +1,25 @@
+<?php
+// +----------------------------------------------------------------------
+// | WSHOTO [ 技术主导，服务至上，提供微信端解决方案 ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2020 http://www.wshoto.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: yc <yc@yuanxu.top>
+// +----------------------------------------------------------------------
+/**
+ * 自定义函数定义文件
+ *
+*/
+# 插件名称
+$Mname = $_W['current_module']['name'];
+
+define('DS', DIRECTORY_SEPARATOR);
+define('IA_ADDONS', IA_ROOT . DS .'addons');
+define('IA_FRAMEWORK', IA_ROOT . DS .'framework');
+define('IA_Mname', IA_ADDONS . DS . $Mname);
+
+define('IA_YC_STATIC', IA_Mname.'/static');
+define('IA_YC_STATIC_COMMON', IA_YC_STATIC . '/common');
+define('IA_YC_STATIC_CSS', IA_YC_STATIC . '/css');
+define('IA_YC_STATIC_JS', IA_YC_STATIC . '/js');
+define('IA_YC_STATIC_IMAGES', IA_YC_STATIC . '/images');
